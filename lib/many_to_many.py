7f3115d77c47388fdc149864756c1dd5acb14278
Klif <sang.wicklife@student.moringaschool.com>
2024-03-13 21:26:17 +0300

    many to many relationships

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,62 @@
+# class Author:
+#     pass
+
+
+# class Book:
+#     pass
+
+
+# class Contract:
+#     pass
+
 class Author:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+
+    def books(self):
+        return list(set([contract.book for contract in self.contracts()]))
+
+    def sign_contract(self, book, date, royalties):
+        contract = Contract(self, book, date, royalties)
+        return contract
+
+    def total_royalties(self):
+        return sum([contract.royalties for contract in self.contracts()])
 
 
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+
+    def authors(self):
+        return list(set([contract.author for contract in self.contracts()]))
 
 
 class Contract:
-    pass
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author):
+            raise TypeError("author must be an instance of Author")
+        if not isinstance(book, Book):
+            raise TypeError("book must be an instance of Book")
+        if not isinstance(date, str):
+            raise TypeError("date must be a string")
+        if not isinstance(royalties, int):
+            raise TypeError("royalties must be an integer")
+
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        self.__class__.all.append(self)
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        return sorted([contract for contract in cls.all if contract.date == date], key=lambda x: x.date)
